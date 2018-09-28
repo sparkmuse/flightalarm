9bbc9558dabbaef8630b0b412e9d17fe1d600780
Alfredo Lopez <alfredlopezf@hotmail.com>
2018-09-28 16:08:17 +0200

    First commit

---
+++ src/main/java/con/github/sparkmuse/flightalarm/FlightalarmApplication.java
@@ -0,0 +1,12 @@
+package con.github.sparkmuse.flightalarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightalarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightalarmApplication.class, args);
+	}
+}
---
+++ src/test/java/con/github/sparkmuse/flightalarm/FlightalarmApplicationTests.java
@@ -0,0 +1,16 @@
+package con.github.sparkmuse.flightalarm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightalarmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
